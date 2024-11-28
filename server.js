0dc80627a59e15a317e72add80308b9202fddf0f
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2024-11-28 20:32:32 +0630

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import mysql from 'mysql2'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import cors from 'cors';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
@@ -14,6 +14,7 @@ app.use(cors({
     methods:["POST","GET"],
     credentials:true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -25,14 +26,14 @@ app.use(session({
         secure:false,
         maxAge:1000 * 60 * 60 * 24
     }
-}))
+}));
 
 const dbConnection = mysql.createPool({
     host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
     password:process.env.MYSQL_PASSWORD,
     database:process.env.MYSQL_DATABASE
-})
+});
 
 app.post('/signup',(req,res) => {
     const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
@@ -47,7 +48,7 @@ app.post('/signup',(req,res) => {
         }
         return res.json(result);
     })
-})
+});
 
 app.post('/login',(req,res) => {
     const sql = "SELECT * FROM login WHERE email = ? and password = ?";
@@ -62,7 +63,7 @@ app.post('/login',(req,res) => {
             return res.json({Login: false})
         }
     })
-})
+});
 
 app.get('/',(req,res) => {
     if(req.session.name){
@@ -70,8 +71,8 @@ app.get('/',(req,res) => {
     }else{
         return res.json({valid:false})
     }
-})
+});
 
 app.listen(8081, () => {
     console.log("Listening.....");
-}) 
+}) ;
